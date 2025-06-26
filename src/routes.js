02e4639e1e32bd32484f7f369fe45a5a2e3651cd
grover-cloud <guilherme.rover@gmai.com>
2025-06-26 14:56:10 -0300

    Resolvendo conflitos

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const LitoralController = require('./controllers/LitoralController');
-const PrevisaoOndaController = require('./controllers/PrevisaoOndaController');
-const Controller = require('./controllers/Controller');
-const PraiaController = require('./controllers/PraiaController');
+const BeachController = require('./controllers/BeachController');
+const ForecastController = require('./controllers/ForecastController');
 
-router.use('/litoral', LitoralController);
-router.use('/onda', PrevisaoOndaController);
-router.use('/praias', PraiaController);
-router.use('/', Controller);
+// Rotas conforme especificado:
+// GET /beaches
+// GET /beaches/:state
+// GET /beaches/:state/:nome
+// GET /beach/:nome/forecast
+// POST /beaches/register
+// POST /beaches/register-all
+// DELETE /beaches/:nome
+
+router.get('/beaches', BeachController.listAll);
+router.get('/beaches/:state', BeachController.listByState);
+router.get('/beaches/:state/:nome', BeachController.getByStateAndName);
+router.get('/beach/:nome/forecast', ForecastController.getForecast);
+router.post('/beaches/register', BeachController.register);
+router.post('/beaches/register-all', BeachController.registerAll);
+router.delete('/beaches/:nome', BeachController.remove);
 
 module.exports = router;
