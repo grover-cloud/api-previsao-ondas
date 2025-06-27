1129b7a215f62c32eef3a8ea74f17e18f92f8394
grover-cloud <guilherme.rover@gmai.com>
2025-06-27 20:39:45 -0300

    Resolvendo conflitos

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const MarineController = require('./controllers/MarineController');
-
 
 const BeachController = require('./controllers/BeachController');
 const ForecastController = require('./controllers/ForecastController');
