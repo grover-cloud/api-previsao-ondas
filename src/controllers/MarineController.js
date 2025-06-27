e2d6dfe2940899104cad85ba645b577939efd8a6
grover-cloud <guilherme.rover@gmai.com>
2025-06-27 20:34:11 -0300

    Resolvendo conflitos

---
+++ src/controllers/MarineController.js
@@ -0,0 +1,13 @@
+const marineService = require('../services/marineService');
+
+const getFauna = async (req, res) => {
+  try {
+    const fauna = await marineService.getFaunaDataBrazil();
+    res.status(200).json(fauna);
+  } catch (error) {
+    console.error('[Erro em getFauna]', error);
+    res.status(500).json({ message: 'Erro ao buscar dados da fauna marinha' });
+  }
+};
+
+module.exports = { getFauna };
